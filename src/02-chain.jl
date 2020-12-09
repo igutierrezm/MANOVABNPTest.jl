af9d1628db55943f149b53c8be784d8a07317245
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2020-12-09 16:08:29 -0300

    Correct the use of the rng in the chain state

--- src/02-chain.jl
+++ src/02-chain.jl
@@ -6,11 +6,11 @@
     J::Int
     K̄::Int = 64
     K::Vector{Int}       = [5]
-    τ::Vector{Int}       = 1:N
+    rng::MersenneTwister = Random.MersenneTwister()
+    τ::Vector{Int}       = randperm(rng, N)
     γ::Vector{Int}       = [0; ones(Int, J - 1)]
-    z::Vector{Int}       = rand(rng, 1:K[1], N)
+    z::Vector{Int}       = [1:K[1]; rand(rng, 1:K[1], N - K[1])]
     U::Set{Int}          = Set(K[1]+1:K̄-1)
     O::Set{Int}          = Set(1:K[1])
     α::Vector{Float64}   = [1.0]
-    rng::MersenneTwister = Random.MersenneTwister()
 end
